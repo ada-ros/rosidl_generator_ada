411c0dd2e625a11709386fb49c741a0ae06c048b
Alejandro R. Mosteo <amosteo@unizar.es>
2021-03-13 17:04:23 +0100

    Ready for proper symbol finding

--- gpr_generator/src/rosidl-generator.adb
+++ gpr_generator/src/rosidl-generator.adb
@@ -1,15 +1,20 @@
 with Ada.Command_Line; use Ada.Command_Line;
+with Ada.Directories; use Ada.Directories;
 with Ada.Text_IO; use Ada.Text_IO;
 
-with C_Strings;
+with Ament.Index;
+
+--  with C_Strings;
+
+with GNAT.OS_Lib; use GNAT.OS_Lib;
 
 --  with ROSIDL.Typesupport;
 
 procedure ROSIDL.Generator is
 
-   function Query (Pack : C_Strings.Chars_Ptr) return C_Strings.Chars_Ptr
-     with Import, Convention => C,
-       External_Name => "rosidl_ada_find_support_lib";
+   --  function Query (Pack : C_Strings.Chars_Ptr) return C_Strings.Chars_Ptr
+   --    with Import, Convention => C,
+   --      External_Name => "rosidl_ada_find_support_lib";
 
 begin
    Put_Line ("GENERATOR INVOKED, HALLELUJAH, HALLELUJAH");
@@ -18,7 +23,13 @@ begin
       Put_Line ("#" & I'Image & ": " & Argument  (I));
    end loop;
 
+   --  Put_Line ("TEST LIB: "
+   --          & C_Strings.Value (Query
+   --            (C_Strings.To_C ("rclada").To_Ptr)));
+
+   Put_Line ("AMENT_PREFIX_PATH="
+             & Getenv ("AMENT_PREFIX_PATH").all);
+   Put_Line ("CWD=" & Current_Directory);
    Put_Line ("TEST LIB: "
-             & C_Strings.Value (Query
-               (C_Strings.To_C ("builtin_interfaces").To_Ptr)));
+             & Ament.Index.Find_Library ("librclada__rosidl_typesupport_c.so"));
 end ROSIDL.Generator;
--- gpr_rosidl/src/rosidl-support.adb
+++ gpr_rosidl/src/rosidl-support.adb
@@ -3,6 +3,8 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 with DL;
 
+with GNAT.OS_Lib; use GNAT.OS_Lib;
+
 with System.Address_Image;
 
 package body ROSIDL.Support is
@@ -17,6 +19,9 @@ package body ROSIDL.Support is
       Handle : constant DL.Handle :=
                  DL.Open ("lib" & Name & "__rosidl_typesupport_c.so");
    begin
+      Put_Line ("AMENT_PREFIX_PATH="
+                & Getenv ("AMENT_PREFIX_PATH").all);
+
       return Addr : constant System.Address := DL.Sym (Handle, Name) do
          if Addr = System.Null_Address then
             raise Program_Error with "Symbol [" & Name & "] could not be found";
